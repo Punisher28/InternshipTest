d9c8432647f134c6fac5f299e8ca47e978a3b670
Punisher28 <slavkokomandir@gmail.com>
2018-10-23 22:46:29 +0300

    First run. Create folder 'production'

--- src/main/Application.java
+++ src/main/Application.java
@@ -10,7 +10,6 @@ public class Application {
         university.addStudent(new Student("Andrew Kostenko"));
         university.addStudent(new Student("Julia Veselkina"));
         university.addStudent(new Student("Maria Perechrest"));
-
         Internship internship = new Internship("Interlink");
         System.out.println("List of internship's students:");
         System.out.println(internship.getStudents());
