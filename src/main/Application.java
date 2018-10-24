035802b094ce460e9a5a2319ea3e799fbb500c80
Punisher28 <slavkokomandir@gmail.com>
2018-10-24 11:08:26 +0300

    Connect JDBC

--- src/main/Application.java
+++ src/main/Application.java
@@ -4,14 +4,39 @@ import institution.University;
 import institution.interlink.Internship;
 import person.Student;
 
+import java.sql.*;
+
 public class Application {
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/students?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String user = "root";
+    private static final String password = "";
+    private static Connection con;
+    private static Statement stmt;
+    private static ResultSet rs;
+
     public static void main(String[] args) {
-        University university = new University("CH.U.I.");
+        String query = "select * from list";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Відсутній JDBC");
+            e.printStackTrace();
+            return;
+        }
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(query);
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+
+        }
+        /*University university = new University("CH.U.I.");
         university.addStudent(new Student("Andrew Kostenko"));
         university.addStudent(new Student("Julia Veselkina"));
         university.addStudent(new Student("Maria Perechrest"));
         Internship internship = new Internship("Interlink");
         System.out.println("List of internship's students:");
-        System.out.println(internship.getStudents());
+        System.out.println(internship.getStudents());*/
     }
 }
